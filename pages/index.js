2f1412c14035bdf2b2a5106af5acb36254a271ae
chrabal <64665475+chrabal@users.noreply.github.com>
2022-11-05 16:46:55 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,15 @@ export default function Home() {
       <h2>It is a lot of fun to learn HTML and JS and React</h2>
     </div>
 
-    <div>
+   <div className={styles.container}>
       <h1>fun</h1>
     </div>
 
-    <div>
+   <div className={styles.container}>
       <h1>fun2</h1>
     </div>
 
-    <div>
+   <div className={styles.container}>
       <h1>fun3</h1>
     </div>
 
