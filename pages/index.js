523cd3eb8e094ed942a99d0f0279e2e213b92b8f
chrabal <64665475+chrabal@users.noreply.github.com>
2022-11-05 16:58:13 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
       <h2>Hello, my name is Craig Hrabal</h2>
       <h1>I am learning!</h1>
       <h2>It is a lot of fun to learn HTML and JS and React</h2>
-        const myElement = <h1>I Love JSX!</h1>;
-    </div>
+    </section>
  
   )
 }
