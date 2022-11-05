645fde92d1772ece6dec6c15009cca8df37c36f1
chrabal <64665475+chrabal@users.noreply.github.com>
2022-11-05 17:11:10 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,5 @@ export default function Home() {
       <h1>I am learning!</h1>
       <h2>It is a lot of fun to learn HTML and JS and React</h2>
     </section>
- 
-  )
-  export default function Home() {
-  return (
-    <section className={styles.container}>
-      <h2>Hello, my name is Craig Hrabal</h2>
-      <h1>I am learning!</h1>
-      <h2>It is a lot of fun to learn HTML and JS and React</h2>
-    </section>
- 
   )
 }
