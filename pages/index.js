b0cb4097ee6faec154d6108b336da00907de6e5d
chrabal <64665475+chrabal@users.noreply.github.com>
2022-11-05 16:50:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,10 @@ export default function Home() {
       <h1>I am learning!</h1>
       <h2>It is a lot of fun to learn HTML and JS and React</h2>
     </div>
-
-   <div className={styles.container}>
-      <h1>fun</h1>
-    </div>
-
-   <div className={styles.container}>
-      <h1>fun2</h1>
-    </div>
-
-   <div className={styles.container}>
-      <h1>fun3</h1>
+    <div className={styles.container}>
+      <h2>Hello, my name is Craig Hrabal</h2>
+      <h1>I am learning!</h1>
+      <h2>It is a lot of fun to learn HTML and JS and React</h2>
     </div>
   )
 }
